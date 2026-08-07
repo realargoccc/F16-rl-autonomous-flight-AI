e75b7a8282edaec47ae2298849802f20e6b33e8c
realargoccc <realargoccc@gmail.com>
2026-08-07 16:09:41 -0700

    changed log std from -0.7 to -1.2, so it reduce the exploration, therefore tends to do less jittery maneuvers

--- train.py
+++ train.py
@@ -7,10 +7,10 @@ from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize, SubprocV
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.env_util import make_vec_env
 
-model_load = "ppo_f16_eleva_v2.6.1.zip"         #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
-model_path = "ppo_f16_eleva_v2.6.2.zip" 
-vecnorm_load = "vecnorm_eleva_v2.6.1.pkl"       #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
-vecnorm_path = "vecnorm_eleva_v2.6.2.pkl"
+model_load = "ppo_f16_eleva_v2.6.2.zip"         #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
+model_path = "ppo_f16_eleva_v2.6.3.zip" 
+vecnorm_load = "vecnorm_eleva_v2.6.2.pkl"       #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
+vecnorm_path = "vecnorm_eleva_v2.6.3.pkl"
 
 if __name__ == "__main__":
     check_env(F16Env())
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     #model = PPO("MlpPolicy", env, verbose = 1, n_steps=512, batch_size=1024, gamma = 0.997, ent_coef = 0.03, tensorboard_log="./tb_logs/") #ent_coef controls how much PPO encourage exploration 
 
     with torch.no_grad():
-        model.policy.log_std.fill_(-0.7) # hard set the explore of each axis to 0.5 (avoiding bang bang)
+        model.policy.log_std.fill_(-1.2) # hard set the explore of each axis to 0.5 (avoiding bang bang)
     model.learn(total_timesteps= 3_000_000,reset_num_timesteps=False, tb_log_name="v2.3.0") #reset_num_timesteps=False (Add when train continous, remove when train fresh)
     model.save(model_path)
     env.save(vecnorm_path)
