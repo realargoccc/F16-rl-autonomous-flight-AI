6371ca2114d48ef58a388c282ed76dd1095ee6f6
realargoccc <realargoccc@gmail.com>
2026-08-20 16:29:05 -0700

    randomize defense and offense

--- flight_env.py
+++ flight_env.py
@@ -156,6 +156,7 @@ class F16Env(gym.Env):
         self.climb_deg = 15.0
         self.dive_deg = 8.0
         self.aim_width = np.radians(20.0)
+        self.defensive_p = 0.0
         #opponent pool
         self.foe_pool = []
         self.foe_pool_prob = 0.5
@@ -243,6 +244,10 @@ class F16Env(gym.Env):
 
         #Foe spawn configs
         foe_spawn_low, foe_spawn_high = self.np_random.choice([(-500.0, -250.0), (250.0, 500.0)])
+        #shuffle defense and offense spawn
+        side = -1.0 if self.np_random.random() < self.defensive_p else 1.0
+        self.setup = "defensive" if side < 0 else "offensive" 
+        
         foe_rel_alt = self.np_random.uniform(foe_spawn_low, foe_spawn_high)
         self.foe['ic/lat-gc-deg'] = lat0 + foe_range / 111320.0
         self.foe['ic/long-gc-deg'] = lon0 + foe_east / (111320.0 * np.cos(np.radians(lat0)))
