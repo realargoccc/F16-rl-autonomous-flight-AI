925a90339d7b7d48cb652aad749e6660e4f2e487
realargoccc <realargoccc@gmail.com>
2026-09-03 16:21:58 -0700

    adding helper function for posture

--- reward_functions/base.py
+++ reward_functions/base.py
@@ -1,6 +1,16 @@
 from collections import namedtuple
+import math
 StepComp = namedtuple("StepComp", ["speed_knots", "curr_g", "alt_agl_m", "crashed", "deck_hit", "truncated",
                                     "dmg_foe", "dmg_me", "foe_boresight"])
+
+'''range halper for posture range'''
+
+def range_window(env, range):
+    if range < env.gun_rmin:
+        return math.exp(-(env.gun_rmin - range) / env.close_width)
+    if range > env.gun_rmax:
+        return math.exp(-(range - env.gun_rmax) / env.range_width)
+    return 1.0
 
 class BaseReward:
     scale = 1.0
