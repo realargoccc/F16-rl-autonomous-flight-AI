d2215450a106f4df1cbcaeab65af01235318a22d
realargoccc <realargoccc@gmail.com>
2026-09-04 15:27:16 -0700

    setting deck reward function

--- reward_functions/deck.py
+++ reward_functions/deck.py
@@ -1,5 +1,17 @@
 from .base import BaseReward
+import numpy as np
 
 class Deck(BaseReward):
     def raw(self, env, computed):
-        
+        alt_km = computed.alt_agl_m / 1000.0
+        sink_mach = -env.me['velocities/h-dot-fps'] * 0.3048 / 340.0 #positive climbing 
+
+        pv = 0.0
+        if alt_km <= env.safe_altitude:
+            pv = -np.clip(sink_mach / env.k_sink * (env.safe_altitude - alt_km) / env.safe_altitude, 0.0, 1.0)
+
+        ph = 0.0
+        if alt_km <= env.danger_altitude:
+            ph = np.clip(alt_km / env.danger_altitude, 0.0, 1.0) - 1.0 - 1.0
+
+        return pv, ph
--- reward_functions/posture.py
+++ reward_functions/posture.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 from .base import BaseReward, range_window
 
-def get_AO_TA_R(me_pos, me_vel, foe_pos, foe_vel):
+def ao_ta_range(me_pos, me_vel, foe_pos, foe_vel):
     los = foe_pos - me_pos
     range_m = np.linalg.norm(los)
     me_speed = np.linalg.norm(me_vel)
@@ -21,8 +21,15 @@ class Posture(BaseReward):
         super().__init__(env)
         self.scale = env.k_bridge
 
-    def _orientation(self, env, computed):
-        
+    def _orientation(self, ao, ta):
+        return 1.0 / (50.0 * ao / np.pi + 2.0) + 0.5 \
+                + min(np.arctanh(1.0 - max(2.0 * ta / np.pi, 1e-4)) / (2.0 * np.pi), 0.0) + 0.5
+
+    def _range(self, range_km):
+        return 1.0 * (range_km < 5) \
+                + (range_km >=5) * np.clip(-0.032 * range_km ** 2 + 0.284 * range_km + 0.38, 0.0, 1.0) \
+                + np.clip(np.exp(-0.16 * range_km), 0.0, 0.2)
 
     def raw(self, env, computed):
-        return self._orientation(env, computed) * range_window(env, env.range)
+        ao, ta, range_m = ao_ta_range(env.me.pos(), env.me.vel(), env.foe.pos(), env.foe.vel())
+        return self._orientation(ao, ta) * self._range(range_m / 1000.0)
