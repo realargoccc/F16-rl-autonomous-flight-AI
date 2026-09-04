9d4c66082964888699149f862af02559c8909646
realargoccc <realargoccc@gmail.com>
2026-09-04 16:15:41 -0700

    +terminal.py for win lose deck hit etc scenarios

---
+++ reward_functions/terminal.py
@@ -0,0 +1,11 @@
+from .base import BaseReward
+
+class Terminal(BaseReward):
+    '''win lose conditions'''
+    def raw(env, self, computed):
+        r = 0.0
+        if computed.deck_hit: r -= env.k_crash
+        if computed.crashed:  r -= env.k_crash
+        if env.foe_hp - computed.dmg_foe <= 0.0: r += env.k_win
+        if env.agent_hp - computed.dmg_me <= 0.0: r -= env.k_win
+        return r
