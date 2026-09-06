f5b6289ed42419a76a593bbd57abec31c62022a1
realargoccc <realargoccc@gmail.com>
2026-09-06 15:14:57 -0700

    code ready for 402 train

--- train.py
+++ train.py
@@ -9,9 +9,9 @@ from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.env_util import make_vec_env
 
 model_load = "ppo_f16_eleva_v4.0.0.zip"         #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
-model_path = "ppo_f16_eleva_v4.0.1.zip" 
+model_path = "ppo_f16_eleva_v4.0.2.zip" 
 vecnorm_load = "vecnorm_eleva_v4.0.0.pkl"       #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
-vecnorm_path = "vecnorm_eleva_v4.0.1.pkl"
+vecnorm_path = "vecnorm_eleva_v4.0.2.pkl"
 
 #selfplay 
 snapshot_every = 500_000
@@ -55,24 +55,24 @@ def make_env():
 if __name__ == "__main__":
     check_env(F16Env())
     env = SubprocVecEnv([make_env for _ in range(8)])   #auto wrap 
-    '''
+    
     env = VecNormalize(         #COMMEWNT OUT WHEN TRAIN CONTINUOUS, UNCOMMENT WHEN TRAIN FRESH
         env, 
         norm_obs=True,          #normalize observations
         norm_reward=False,      #DO NOT normalize reward since they are specifically assigned
         clip_obs=10.0           #cap the upper and lower limit between -10 - 10
     )
-    '''
-    env = VecNormalize.load(vecnorm_load, env)  #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
-    env.training = True                         #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
-    env.norm_reward = False                     #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
+    
+    #env = VecNormalize.load(vecnorm_load, env)  #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
+    #env.training = True                         #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
+    #env.norm_reward = False                     #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
 
     #tensorboard --logdir=./tb_logs/
-    model = PPO.load(model_load, env=env, ent_coef = 0.002, verbose = 1, tensorboard_log="./tb_logs/")
-    #model = PPO("MlpPolicy", env, verbose = 1, n_steps=512, batch_size=1024, gamma = 0.997, ent_coef = 0.03, tensorboard_log="./tb_logs/") #ent_coef controls how much PPO encourage exploration 
+    #model = PPO.load(model_load, env=env, ent_coef = 0.002, verbose = 1, tensorboard_log="./tb_logs/")
+    model = PPO("MlpPolicy", env, verbose = 1, n_steps=512, batch_size=1024, gamma = 0.997, ent_coef = 0.03, tensorboard_log="./tb_logs/") #ent_coef controls how much PPO encourage exploration 
 
-    pool = SelfPlayPool(every=snapshot_every, prefix="v4.0.1", warmup=snapshot_warm, prob=pool_prob)
-    model.learn(total_timesteps= 3_000_000, callback=pool, reset_num_timesteps=False, tb_log_name="v4.0.1") #reset_num_timesteps=False (Add when train continous, remove when train fresh)
+    pool = SelfPlayPool(every=snapshot_every, prefix="v4.0.2", warmup=snapshot_warm, prob=pool_prob)
+    model.learn(total_timesteps= 3_000_000, callback=pool, tb_log_name="v4.0.2") #reset_num_timesteps=False (Add when train continous, remove when train fresh)
     model.save(model_path)
     env.save(vecnorm_path)
 
