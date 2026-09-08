7c7575fad363f0946596a945da519ecbcb93e870
realargoccc <realargoccc@gmail.com>
2026-09-08 11:44:21 -0700

    trained results of 407

--- train.py
+++ train.py
@@ -8,10 +8,10 @@ from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize, SubprocV
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.env_util import make_vec_env
 
-model_load = "ppo_f16_eleva_v4.0.5.zip"         #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
-model_path = "ppo_f16_eleva_v4.0.6.zip" 
-vecnorm_load = "vecnorm_eleva_v4.0.5.pkl"       #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
-vecnorm_path = "vecnorm_eleva_v4.0.6.pkl"
+model_load = "ppo_f16_eleva_v4.0.6.zip"         #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
+model_path = "ppo_f16_eleva_v4.0.7.zip" 
+vecnorm_load = "vecnorm_eleva_v4.0.6.pkl"       #COMMEWNT OUT WHEN TRAIN FRESH, UN COMMENT WHEN TRAIN CONTINUOUS
+vecnorm_path = "vecnorm_eleva_v4.0.7.pkl"
 
 #selfplay 
 snapshot_every = 500_000
@@ -71,8 +71,8 @@ if __name__ == "__main__":
     model = PPO.load(model_load, env=env, ent_coef = 1e-3, verbose = 1, tensorboard_log="./tb_logs/")
     #model = PPO("MlpPolicy", env, verbose = 1, n_steps=512, batch_size=1024, gamma = 0.997, ent_coef = 1e-3, tensorboard_log="./tb_logs/") #ent_coef controls how much PPO encourage exploration 
 
-    pool = SelfPlayPool(every=snapshot_every, prefix="v4.0.6", warmup=snapshot_warm, prob=pool_prob)
-    model.learn(total_timesteps= 3_000_000, reset_num_timesteps=False, tb_log_name="v4.0.6") #reset_num_timesteps=False (Add when train continous, remove when train fresh)
+    pool = SelfPlayPool(every=snapshot_every, prefix="v4.0.7", warmup=snapshot_warm, prob=pool_prob)
+    model.learn(total_timesteps= 3_000_000, reset_num_timesteps=False, tb_log_name="v4.0.7") #reset_num_timesteps=False (Add when train continous, remove when train fresh)
     model.save(model_path)
     env.save(vecnorm_path)
 
