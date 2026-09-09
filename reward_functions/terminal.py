1ca17100fab42c8d22c54cc312b89faccee6fe02
realargoccc <realargoccc@gmail.com>
2026-09-09 10:52:56 -0700

    retrain 415

--- reward_functions/terminal.py
+++ reward_functions/terminal.py
@@ -6,6 +6,7 @@ class Terminal(BaseReward):
         r = 0.0
         if computed.deck_hit: r -= env.k_crash
         if computed.crashed:  r -= env.k_crash
+        if computed.foe_down: r += env.k_crash
         if env.foe_hp - computed.dmg_foe <= 0.0: r += env.k_win
         if env.agent_hp - computed.dmg_me <= 0.0: r -= env.k_win
         return r
