fe6f5082af74248ed71623541df5c7bbcd8734b2
realargoccc <realargoccc@gmail.com>
2026-09-09 09:52:03 -0700

    trained results of 413

--- csvtotacview.py
+++ csvtotacview.py
@@ -2,7 +2,7 @@ import csv
 import math
 
 csv_path = "eval_worst.csv"
-acmi_path = "f16_intercept_v4.1.2.acmi"
+acmi_path = "f16_intercept_v4.1.3.acmi"
 
 #nellis afb lat and lon (fight location)
 ref_lat = 36.20
