1cf74fdbf52e5948bcca726d73d6e15f2ac99b0f
realargoccc <realargoccc@gmail.com>
2026-09-09 11:37:07 -0700

    remove bang bang controller in deftrain

--- deftrain.py
+++ deftrain.py
@@ -1,5 +1,5 @@
 import torch
-from pidef import make_def_env, F16DefEnv
+from pidef import make_def_env
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.vec_env import SubprocVecEnv, VecNormalize
@@ -11,19 +11,15 @@ vecnorm_load = "vecnorm_eleva_v4.1.5.pkl"
 vecnorm_path = "vecnorm_def_v1.0.0.pkl"
 
 if __name__ == "__main__":
-    check_env(F16DefEnv())
-    env = SubprocVecEnv([make_env for _ in range(8)])
+    check_env(make_def_env().env)
+    env = SubprocVecEnv([make_def_env for _ in range(8)])
     env = VecNormalize.load(vecnorm_load, env)
     env.training = True
     env.norm_reward = False
     #env = VecNormalize(env, norm_obs=True, norm_reward=False, clip_obs=10.0)
 
     #model = PPO("MlpPolicy", env, verbose=1, n_steps=512, batch_size=1024, gamma=0.997, ent_coef=0.03, tensorboard_log="./tb_logs/")
-
-    
-    model = PPO.load(model_load, env=env, ent_coef=0.002, verbose=1, tensorboard_log="./tb_logs/")
-    with torch.no_grad():
-        model.policy.log_std.fill_(-0.7)
+    model = PPO.load(model_load, env=env, ent_coef=1e-3, verbose=1, tensorboard_log="./tb_logs/")
     
     model.learn(total_timesteps=2_000_000,reset_num_timesteps=False, tb_log_name="def_v3.0.0") #reset_num_timesteps=False,
     model.save(model_path)
