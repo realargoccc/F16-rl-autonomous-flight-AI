ecf1091dc1e91ab6a84ebcabf53c7e881c31b753
realargoccc <realargoccc@gmail.com>
2026-09-10 15:16:45 -0700

    trained 101 h2h

--- h2htrain.py
+++ h2htrain.py
@@ -4,10 +4,10 @@ from stable_baselines3.common.vec_env import SubprocVecEnv, VecNormalize
 from pih2h import make_h2h_env
 from train import SelfPlayPool
 
-model_load = "ppo_f16_eleva_v4.1.6.zip"
-model_path = "ppo_f16_eleva_h2h_v1.0.0.zip"
-vecnorm_load = "vecnorm_eleva_v4.1.6.pkl"
-vecnorm_path = "vecnorm_eleva_h2h_v1.0.0.pkl"
+model_load = "ppo_f16_eleva_h2h_v1.0.0.zip"
+model_path = "ppo_f16_eleva_h2h_v1.0.1.zip"
+vecnorm_load = "vecnorm_eleva_h2h_v1.0.0.pkl"
+vecnorm_path = "vecnorm_eleva_h2h_v1.0.1.pkl"
 
 snapshot_every = 500_000    #500k per snapshot (update selfplay version)
 snapshot_warm = 0        
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     model = PPO.load(model_load, env=env, ent_coef=1e-3, verbose=1, tensorboard_log="./tb_logs/")
 
-    pool = SelfPlayPool(every=snapshot_every, prefix="h2h_v1.0.0", warmup=snapshot_warm, prob=pool_prob)
-    model.learn(total_timesteps=3_000_000, callback=pool, reset_num_timesteps=False, tb_log_name="h2h_v1.0.0")
+    pool = SelfPlayPool(every=snapshot_every, prefix="h2h_v1.0.1", warmup=snapshot_warm, prob=pool_prob)
+    model.learn(total_timesteps=3_000_000, callback=pool, reset_num_timesteps=False, tb_log_name="h2h_v1.0.1")
     model.save(model_path)
     env.save(vecnorm_path)
