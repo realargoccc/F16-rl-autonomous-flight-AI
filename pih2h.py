6afa681cae9fc4f56a172038a51bb9716ef40c12
realargoccc <realargoccc@gmail.com>
2026-09-10 10:48:25 -0700

    set up self play train env for the h2h merge

---
+++ pih2h.py
@@ -0,0 +1,17 @@
+from flight_env import F16Env
+from stable_baselines3.common.monitor import Monitor
+
+ASPECT_BAND = (150.0, 180.0)
+RANGE_BAND = (2500.0, 4000.0)
+
+def make_h2h_env(pool=(), aspect_band = ASPECT_BAND, range_band = RANGE_BAND):
+    env = F16Env()
+    env.aspect_band = ASPECT_BAND
+    env.range_band = RANGE_BAND
+    env.defensive_p = 0.0
+    env.selfplay = "pfsp"
+    for tag in pool:
+        env.load_foe(tag)
+    env.foe_pool_prob = 1.0
+
+    return Monitor(env, info_keywords=("crashed", "foe_crashed", "win", "deck_hit", "foe_down"))
